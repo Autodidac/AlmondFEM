7ed12624a7a9f667f93a0624225953a94f590497
Autodidac <iammidnightman@hotmail.com>
2025-10-30 07:07:45 -0400

    update

--- shared/safe_io/include/safe_io/utils.hpp
+++ shared/safe_io/include/safe_io/utils.hpp
@@ -1,29 +1,55 @@
-#pragma once
+﻿#pragma once
 
-#include <fmt/core.h>
-#include <iostream>
-#include <utility>
+#include <fmt/format.h>   // println, format_string, format
+#include <fmt/ostream.h>  // enable fmt for types with operator<<
+#include <iostream>       // std::cout
+#include <string>
+#include <string_view>
 
-namespace safe_io
-{
-    namespace detail
-    {
-        // Platform specific setup to keep console output UTF-8 friendly.
+namespace safe_io {
+    namespace detail {
         void configure_console() noexcept;
     }
 
-    // Returns a reference to std::cout for safe, global access to output.
-    inline std::ostream& out() noexcept
-    {
+    // Optional: keep a stable ostream handle if other code uses it.
+    inline std::ostream& out() noexcept {
         detail::configure_console();
         return std::cout;
     }
 
-    // A wrapper function for formatted output using {fmt}.
-    // This function allows type-safe, efficient formatting with automatic newline.
-    template <typename... Args>
-    void print(fmt::format_string<Args...> fmt_str, Args&&... args)
-    {
-        out() << fmt::vformat(fmt_str, fmt::make_format_args(std::forward<Args>(args)...)) << '\n';
+    // Format → std::string.
+    template <class... Args>
+    [[nodiscard]] inline std::string sformat(fmt::format_string<Args...> fmt_str, Args&&... args) {
+        detail::configure_console();
+        return fmt::format(fmt_str, std::forward<Args>(args)...);
     }
+
+    // Print to stdout with newline. Perfect-forward args. No arg-store, no vformat.
+    template <class... Args>
+    inline void print(fmt::format_string<Args...> fmt_str, Args&&... args) {
+        detail::configure_console();
+        fmt::println(stdout, fmt_str, std::forward<Args>(args)...);
+        std::fflush(stdout);
+    }
+
+    // Print to stderr with newline.
+    template <class... Args>
+    inline void eprint(fmt::format_string<Args...> fmt_str, Args&&... args) {
+        detail::configure_console();
+        fmt::println(stderr, fmt_str, std::forward<Args>(args)...);
+        std::fflush(stderr);
+    }
+
+    // Print to stderr and abort.
+    template <class... Args>
+    [[noreturn]] inline void fatal(fmt::format_string<Args...> fmt_str, Args&&... args) {
+        detail::configure_console();
+        fmt::println(stderr, fmt_str, std::forward<Args>(args)...);
+        std::fflush(stderr);
+        std::terminate();
+    }
+
 } // namespace safe_io
+
+// If you use fmt::join / ranges anywhere, include this in that TU:
+//   #include <fmt/ranges.h>
--- shared/safe_io/src/utils.cpp
+++ shared/safe_io/src/utils.cpp
@@ -4,42 +4,39 @@
 #include <iostream>
 
 #ifdef _WIN32
-#    include <Windows.h>
-#    include <fcntl.h>
-#    include <io.h>
+#include <Windows.h>
+#include <io.h>
 #endif
 
-namespace safe_io
-{
-    namespace detail
-    {
-        void configure_console() noexcept
-        {
+namespace safe_io {
+    namespace detail {
+
+        void configure_console() noexcept {
 #ifdef _WIN32
             static const bool configured = [] {
-                if (!_isatty(_fileno(stdout)))
-                {
+                // Only tweak real terminals.
+                if (!_isatty(_fileno(stdout)) && !_isatty(_fileno(stderr)))
                     return true;
-                }
 
+                // UTF-8 code pages.
                 ::SetConsoleOutputCP(CP_UTF8);
                 ::SetConsoleCP(CP_UTF8);
 
-                const HANDLE handle = ::GetStdHandle(STD_OUTPUT_HANDLE);
-                if (handle != INVALID_HANDLE_VALUE)
-                {
+                // Enable ANSI/VT sequences for colors.
+                const HANDLE hOut = ::GetStdHandle(STD_OUTPUT_HANDLE);
+                if (hOut != INVALID_HANDLE_VALUE) {
                     DWORD mode = 0;
-                    if (::GetConsoleMode(handle, &mode))
-                    {
-                        ::SetConsoleMode(handle, mode | ENABLE_PROCESSED_OUTPUT | ENABLE_VIRTUAL_TERMINAL_PROCESSING);
+                    if (::GetConsoleMode(hOut, &mode)) {
+                        ::SetConsoleMode(hOut, mode | ENABLE_PROCESSED_OUTPUT | ENABLE_VIRTUAL_TERMINAL_PROCESSING);
                     }
                 }
 
-                _setmode(_fileno(stdout), _O_U8TEXT);
+                // Do NOT switch to _O_U8TEXT. We print narrow UTF-8 via fmt.
                 return true;
-            }();
+                }();
             (void)configured;
 #endif
         }
+
     } // namespace detail
 } // namespace safe_io
